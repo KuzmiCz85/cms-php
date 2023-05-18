2eccdee06dc119effa3175634d9694086d499213
KrejciLukas <lukas.akim@centrum.cz>
2023-05-18 19:56:29 +0200

    Add autoloader

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
-  echo 'Simple website CMS'
+
+// Autoloader
+function autoloader(string $class): void
+{
+  if (preg_match('/Controller$/', $class))
+    require("./app/Controller/" . $class . ".php");
+  else
+    require("./app/Model/" . $class . ".php");
+}
+spl_autoload_register('autoloader');
+
+// Database connection
+$Db = new Db;
+Db::connect("127.0.0.1", "cms_db", "root", "");
+
+echo 'Simple website CMS';
 
 ?>
