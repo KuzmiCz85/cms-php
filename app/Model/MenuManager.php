8c9d4ab7a4244cf942c9446478836104c5e7f99c
KrejciLukas <lukas.akim@centrum.cz>
2023-07-13 12:15:05 +0200

    Edit admin - page slug edit

--- app/Model/MenuManager.php
+++ app/Model/MenuManager.php
@@ -3,17 +3,11 @@ class MenuManager {
 
   public function getItems(): array|bool
   {
-    $pageNames = [];
-
-    $pages = Db::queryAll("
-      SELECT name
+    return Db::queryAll("
+      SELECT name, url_slug
       FROM page
       WHERE in_menu = ?
     ",
     array(true));
-
-    foreach ($pages as $page) array_push($pageNames, $page['name']);
-
-    return $pageNames;
   }
 }
